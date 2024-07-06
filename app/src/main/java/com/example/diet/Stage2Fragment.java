14631d39601014f9c2c634002662b433ff81728a
devlindinh <devlindinh2003@gmail.com>
2024-07-06 13:58:52 +0700

    Build stage 2 goal [KienNDT]

--- app/src/main/java/com/example/diet/Stage2Fragment.java
+++ app/src/main/java/com/example/diet/Stage2Fragment.java
@@ -5,6 +5,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.NumberPicker;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -15,7 +16,8 @@ import com.google.android.material.button.MaterialButton;
 
 public class Stage2Fragment extends Fragment {
 
-    private NumberPicker weightPicker;
+    private NumberPicker weightPickerInteger, weightPickerDecimal;
+    private TextView liveWeightText;
     private double weight;
     private MaterialButton nextStepButton;
 
@@ -24,24 +26,39 @@ public class Stage2Fragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_stage2, container, false);
 
-        weightPicker = view.findViewById(R.id.weightPicker);
+        weightPickerInteger = view.findViewById(R.id.weightPickerInteger);
+        weightPickerDecimal = view.findViewById(R.id.weightPickerDecimal);
+        liveWeightText = view.findViewById(R.id.liveWeightText);
         nextStepButton = getActivity().findViewById(R.id.btnNext);
+
+        // Set up the integer weight picker
+        weightPickerInteger.setMinValue(30);
+        weightPickerInteger.setMaxValue(150);
+        weightPickerInteger.setValue(75);
+
+        // Set up the decimal weight picker
+        weightPickerDecimal.setMinValue(0);
+        weightPickerDecimal.setMaxValue(9);
+        weightPickerDecimal.setValue(0);
+        weightPickerDecimal.setFormatter(value -> String.valueOf(value));
 
         nextStepButton.setEnabled(false);
         nextStepButton.setTextColor(ContextCompat.getColor(getContext(), R.color.gray));
 
-        weightPicker.setMinValue(30);
-        weightPicker.setMaxValue(150);
-        weightPicker.setValue(70);
-        weightPicker.setWrapSelectorWheel(true);
-        weightPicker.setFormatter(value -> String.format("%02d kg", value));
-        weightPicker.setOnValueChangedListener((picker, oldVal, newVal) -> {
-            weight = newVal;
-            nextStepButton.setEnabled(true);
-            nextStepButton.setTextColor(ContextCompat.getColor(getContext(), R.color.mineral_green));
-        });
+        weightPickerInteger.setOnValueChangedListener((picker, oldVal, newVal) -> updateWeight());
+        weightPickerDecimal.setOnValueChangedListener((picker, oldVal, newVal) -> updateWeight());
+
+        // Initialize live weight text
+        updateWeight();
 
         return view;
+    }
+
+    private void updateWeight() {
+        weight = weightPickerInteger.getValue() + weightPickerDecimal.getValue() / 10.0;
+        liveWeightText.setText(String.format("%.1f kg", weight));
+        nextStepButton.setEnabled(true);
+        nextStepButton.setTextColor(ContextCompat.getColor(getContext(), R.color.mineral_green));
     }
 
     public double getWeight() {
