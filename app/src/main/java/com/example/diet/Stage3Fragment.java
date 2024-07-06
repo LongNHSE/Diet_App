54c943f806b76ee04b7ea76faf2932cb24bf4211
devlindinh <devlindinh2003@gmail.com>
2024-07-06 14:05:57 +0700

    Build stage 3 goal [KienNDT]

--- app/src/main/java/com/example/diet/Stage3Fragment.java
+++ app/src/main/java/com/example/diet/Stage3Fragment.java
@@ -5,6 +5,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.NumberPicker;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -16,6 +17,7 @@ import com.google.android.material.button.MaterialButton;
 public class Stage3Fragment extends Fragment {
 
     private NumberPicker heightPicker;
+    private TextView liveHeightText;
     private int height;
     private MaterialButton nextStepButton;
 
@@ -25,22 +27,30 @@ public class Stage3Fragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_stage3, container, false);
 
         heightPicker = view.findViewById(R.id.heightPicker);
+        liveHeightText = view.findViewById(R.id.liveHeightText);
         nextStepButton = getActivity().findViewById(R.id.btnNext);
+
+        // Set up the height picker
+        heightPicker.setMinValue(100);
+        heightPicker.setMaxValue(200);
+        heightPicker.setValue(175);
 
         nextStepButton.setEnabled(false);
         nextStepButton.setTextColor(ContextCompat.getColor(getContext(), R.color.gray));
 
-        heightPicker.setMinValue(100);
-        heightPicker.setMaxValue(250);
-        heightPicker.setValue(170);
-        heightPicker.setWrapSelectorWheel(true);
-        heightPicker.setOnValueChangedListener((picker, oldVal, newVal) -> {
-            height = newVal;
-            nextStepButton.setEnabled(true);
-            nextStepButton.setTextColor(ContextCompat.getColor(getContext(), R.color.mineral_green));
-        });
+        heightPicker.setOnValueChangedListener((picker, oldVal, newVal) -> updateHeight());
+
+        // Initialize live height text
+        updateHeight();
 
         return view;
+    }
+
+    private void updateHeight() {
+        height = heightPicker.getValue();
+        liveHeightText.setText(String.format("%d cm", height));
+        nextStepButton.setEnabled(true);
+        nextStepButton.setTextColor(ContextCompat.getColor(getContext(), R.color.mineral_green));
     }
 
     public int getHeight() {
